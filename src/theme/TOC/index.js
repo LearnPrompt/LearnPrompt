67f6d9ff81e36a01083102f1c4b3936b3491d4be
黄婷婷 <huangtt29@mail2.sysu.edu.cn>
2023-10-29 14:16:52 +0800

    add ads div into TOC

--- src/theme/TOC/index.js
+++ src/theme/TOC/index.js
@@ -1,13 +1,23 @@
-import React, { useEffect } from 'react';
-import TOC from '@theme-original/TOC';
-
-export default function TOCWrapper(props) {
+import React from 'react';
+import clsx from 'clsx';
+import TOCItems from '@theme/TOCItems';
+import styles from './styles.module.css';
+// Using a custom className
+// This prevents TOCInline/TOCCollapsible getting highlighted by mistake
+const LINK_CLASS_NAME = 'table-of-contents__link toc-highlight';
+const LINK_ACTIVE_CLASS_NAME = 'table-of-contents__link--active';
+export default function TOC({className, ...props}) {
   return (
-    <>
-      <TOC {...props} />
+    <div className={clsx(styles.tableOfContents, 'thin-scrollbar', className)}>
+      <TOCItems
+        {...props}
+        linkClassName={LINK_CLASS_NAME}
+        linkActiveClassName={LINK_ACTIVE_CLASS_NAME}
+      />
+
       <div className='wwads-cn wwads-vertical' data-id='290' style={{
         maxWidth: 290
       }}></div>
-    </>
+    </div>
   );
 }
